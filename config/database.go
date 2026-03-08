02eed2ffdcf5f565d20e1ae4bcc37e788ecb3355
Rofiq354 <rofik010206@gmail.com>
2026-03-09 06:22:27 +0700

    fix: database url with neon

--- config/database.go
+++ config/database.go
@@ -11,28 +11,34 @@ import (
 )
 
 func ConnectDatabase() (*gorm.DB, error) {
-	var dsn string
-
-	// Cek DATABASE_URL dulu
-	databaseURL := os.Getenv("DATABASE_URL")
-	if databaseURL != "" {
-		dsn = databaseURL
-	} else {
-		// Fallback ke individual env vars
-		host := os.Getenv("DB_HOST")
-		user := os.Getenv("DB_USER")
-		password := os.Getenv("DB_PASS")
-		dbname := os.Getenv("DB_NAME")
-		port := os.Getenv("DB_PORT")
-
-		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-			host, user, password, dbname, port)
-	}
+	dsn := os.Getenv("DATABASE_URL")
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
+	// var dsn string
+
+	// // Cek DATABASE_URL dulu
+	// databaseURL := os.Getenv("DATABASE_URL")
+	// if databaseURL != "" {
+	// 	dsn = databaseURL
+	// } else {
+	// 	// Fallback ke individual env vars
+	// 	host := os.Getenv("DB_HOST")
+	// 	user := os.Getenv("DB_USER")
+	// 	password := os.Getenv("DB_PASS")
+	// 	dbname := os.Getenv("DB_NAME")
+	// 	port := os.Getenv("DB_PORT")
+
+	// 	dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	// 		host, user, password, dbname, port)
+	// }
+
+	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// if err != nil {
+	// 	return nil, err
+	// }
 
 	err = db.AutoMigrate(
 		&models.Category{},
